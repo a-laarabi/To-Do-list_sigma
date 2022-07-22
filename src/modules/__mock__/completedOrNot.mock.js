ff0d1051958fa49b4e3fc0a2c532b52dc68bae64
anasse Laarabi <anas@MacBook-Pro-de-anasse.local>
2022-07-22 16:20:37 +0100

    Updating comppletedOrNot.mock.js

--- src/modules/__mock__/completedOrNot.mock.js
+++ src/modules/__mock__/completedOrNot.mock.js
@@ -1,7 +1,11 @@
 import localStorage from './localStorage.mock.js';
 
 const completed = (task, index) => {
-  (task[index].completed === true) ? task[index].completed = false : task[index].completed = true;
+  if (task[index].completed === true) {
+    task[index].completed = false;
+  } else {
+    task[index].completed = true;
+  }
   localStorage.setItem('data', task);
   return task;
 };
